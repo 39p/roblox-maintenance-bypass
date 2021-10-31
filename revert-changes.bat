7a33fd84374a8826934f48145c673ad57d3f8583
xhayper <hayper1919@gmail.com>
2021-11-01 04:12:06 +0700

    Update revert-changes.bat

--- revert-changes.bat
+++ revert-changes.bat
@@ -1,4 +1,10 @@
 @echo off
+net session >nul 2>&1
+if %errorLevel% NEQ 0 (
+  echo This file must be ran with administrator privileges
+  pause
+  exit /b 1
+)
 cd %HOMEDRIVE%/Windows/System32/drivers/etc/
 findstr /v 128.116.119. hosts > temp
 type temp > hosts
